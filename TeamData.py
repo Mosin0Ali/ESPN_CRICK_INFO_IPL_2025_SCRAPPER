9a395c00200c5e9cd06d5eeef95455b686ffe198
Mosin Ali Siddiqui <alimosinali007@gmail.com>
2025-03-25 13:45:08 +0545

    Terminal Based Player Data Scrap

--- TeamData.py
+++ TeamData.py
@@ -2,10 +2,60 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 import os
-from datetime import datetime
-import time
-import random
+import sys
+import traceback
 
+
+data_folder_path=f"data"
+
+def load_teams():
+    team_data_folder=f"{data_folder_path}/Team-Data"
+    files=[f for f in os.listdir(team_data_folder) if os.path.isfile(os.path.join(team_data_folder,f))]
+    
+    if not files:
+        print("No files found.")
+        return None
+    
+    print("\nTeam List: \n")
+    for idx, file in enumerate(files,1):
+        print(f"{idx}.{file.replace('.json','').replace('-',' ')}")
+    
+    while True:
+        try:
+            choice= int(input("\n Enter team (number): "))
+            if 1<=choice<=len(files):
+                selected_file= os.path.join(team_data_folder,files[choice-1])
+                team_name = files[choice - 1].replace('.json', '')
+                return selected_file, team_name
+            else:
+                print("\n Invalid choice. Please select a valid number.")
+        except ValueError:
+            print("\n Please enter a valid number.")
+    
+
+def load_players(team_file):
+    try:
+        with open(team_file,"r") as f:
+            team_data=json.load(f)
+        players= team_data.get("players",[])
+        if not players:
+            print("\n No players found in this team:")
+            return None
+        for idx, player in enumerate(players,1):
+            print(f"{idx}.{player['name']}")
+        while True:
+            try:
+                choice = int(input("\nEnter player (number): "))
+                if 1 <= choice <= len(players):
+                    return players[choice - 1]
+                else:
+                    print("\nInvalid choice. Please select a valid number.")
+            except ValueError:
+                print("\nPlease enter a valid number.")
+            
+    except Exception as e:
+        print(f"\n Error reading file :{e}")
+        return None
 
 def format_and_save_player_data(data, team_name):
     try:
@@ -14,7 +64,8 @@ def format_and_save_player_data(data, team_name):
             2: 'ODI',
             3: 'T20I',
             6: 'T20s',
-            5: 'List A'
+            5: 'List A',
+            6:'Other'
         }
 
         player_profile = data['player']
@@ -35,8 +86,6 @@ def format_and_save_player_data(data, team_name):
                 debut_matches.append(temp)
                 format_done.append(temp['format'])
 
-        
-
         player = {
             'name': player_profile['longName'],
             'dob': f"{player_profile['dateOfBirth']['year']}-{player_profile['dateOfBirth']['month']}-{player_profile['dateOfBirth']['date']}",
@@ -51,8 +100,8 @@ def format_and_save_player_data(data, team_name):
             'photo':player_profile['headshotImageUrl']
         }
        
-
         player_stats = data['content']['careerAverages']['stats']
+       
         recent_matches = data['content']['matches']['types'][0]['recent']
         recent_performances = []
 
@@ -72,130 +121,95 @@ def format_and_save_player_data(data, team_name):
         player['recent'] = recent_performances
 
         temp_stat = {}
+      
         for stat in player_stats:
-            temp = {}
-            format_type = cl_array[stat['cl']]
-            stat_type = stat['type']
             
-            if stat_type == 'BATTING':
-                temp.update({
-                    'matches': stat['mt'],
-                    'innings': stat['in'],
-                    'runs': stat['rn'],
-                    'balls_faced': stat['bl'],
-                    'average': stat['avg'],
-                    'strike_rate': stat['sr'],
-                    'notouts': stat['no'],
-                    'fours': stat['fo'],
-                    'sixes': stat['si'],
-                    'highest_score': stat['hs'],
-                    'hundreds': stat['hn'],
-                    'fifties': stat['ft'],
-                    'catches': stat['ct'],
-                    'stumpings': stat['st']
-                })
-            elif stat_type == 'BOWLING':
-                temp.update({
-                    'matches': stat['mt'],
-                    'innings': stat['in'],
-                    'runs_conceeded': stat['rn'],
-                    'balls_bowled': stat['bl'],
-                    'average': stat['avg'],
-                    'strike_rate': stat['sr'],
-                    'wickets': stat['wk'],
-                    'best_figure': stat['bbi'],
-                    'four_wickets': stat['fwk'],
-                    'five_wickets': stat['fw'],
-                    'ten_wickets': stat['tw'],
-                    'economy': stat['bwe']
-                })
-            temp_stat.setdefault(format_type, {})[stat_type] = temp
-
+            temp = {}
+            format_type = cl_array.get(stat['cl'], None) 
+            if format_type is not None:
+                stat_type = stat['type']
+                if stat_type == 'BATTING':
+                    temp.update({
+                        'matches': stat['mt'],
+                        'innings': stat['in'],
+                        'runs': stat['rn'],
+                        'balls_faced': stat['bl'],
+                        'average': stat['avg'],
+                        'strike_rate': stat['sr'],
+                        'notouts': stat['no'],
+                        'fours': stat['fo'],
+                        'sixes': stat['si'],
+                        'highest_score': stat['hs'],
+                        'hundreds': stat['hn'],
+                        'fifties': stat['ft'],
+                        'catches': stat['ct'],
+                        'stumpings': stat['st']
+                    })
+                elif stat_type == 'BOWLING':
+                    temp.update({
+                        'matches': stat['mt'],
+                        'innings': stat['in'],
+                        'runs_conceeded': stat['rn'],
+                        'balls_bowled': stat['bl'],
+                        'average': stat['avg'],
+                        'strike_rate': stat['sr'],
+                        'wickets': stat['wk'],
+                        'best_figure': stat['bbi'],
+                        'four_wickets': stat['fwk'],
+                        'five_wickets': stat['fw'],
+                        'ten_wickets': stat['tw'],
+                        'economy': stat['bwe']
+                    })
+                temp_stat.setdefault(format_type, {})[stat_type] = temp
+                
         player['stats'] = temp_stat
         player_json = json.dumps(player)
-
-        folder = os.path.join('..', 'data', team_name)
+        folder = os.path.join('data', team_name)
         os.makedirs(folder, exist_ok=True)
         file_path = os.path.join(folder, f"{data['player']['objectId']}.json")
-
         with open(file_path, 'w') as f:
             f.write(player_json)
-
-        save_updated_at(team_name)
         return True
 
     except Exception as e:
         print(f"Error: {e}")
-        return False
-
-
-def save_updated_at(team_name):
-    file_path = f'data/teams_updated.json'
-    if os.path.exists(file_path):
+        print("Error:", e)
+        print("Traceback:")
+        traceback.print_exc()
+        return False  
+  
+selected_team_file,team_name = load_teams()
+if selected_team_file and team_name:
+    selected_player = load_players(selected_team_file)
+    if selected_player:
+        print("Loading player ....")
+        player_data_url = f"https://www.espncricinfo.com/cricketers/{selected_player['name'].lower().replace(' ', '-')}-{selected_player['id']}"
+        print(player_data_url)
+        options= webdriver.ChromeOptions()
+        options.headless=False
+        driver=webdriver.Chrome(options=options)
+        driver.get(player_data_url)
         try:
-            with open(file_path, 'r') as f:
-                json_data = f.read()
-                data_array = json.loads(json_data)
-                if not isinstance(data_array, list):
-                    data_array = []
-        except (json.JSONDecodeError, FileNotFoundError):
-            data_array = []
-    else:
-        data_array = []
-
-    updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    found = False
-
-    for team in data_array:
-        if team.get('team_name') == team_name:
-            team['updated_at'] = updated_at
-            found = True
-            break
-
-    if not found:
-        data_array.append({
-            'team_name': team_name,
-            'updated_at': updated_at
-        })
+            player_content = driver.find_element(By.ID, "__NEXT_DATA__")
+            player_content = player_content.get_attribute('innerHTML')
+            player_content=json.loads(player_content)
+            player_content=player_content['props']['appPageProps']['data']
+            print('Writing data for '+ team_name)
+            format_and_save_player_data(player_content, team_name)
+            print("File written successfully !")
+            sys.exit()
+        except Exception as e:
+            print(f"Error fetching data for {player_data_url}: {e}")
+            sys.exit()
         
-    try:
-        with open(file_path, 'w') as f:
-            json.dump(data_array, f, indent=4)
-    except Exception as e:
-        print(f"Error saving updated time: {e}")          
+        
         
         
 
-player_data_url="https://www.espncricinfo.com/cricketers/"
-options= webdriver.ChromeOptions()
-options.headless=False
-driver=webdriver.Chrome(options=options)
-data_path="data/"
-team_data_folder=f"{data_path}Team-Data"
 
-file_names_with_extensions = [filename for filename in os.listdir(team_data_folder) if os.path.isfile(os.path.join(team_data_folder, filename))]
 
-for team in file_names_with_extensions:
-    file_path=f"{team_data_folder}/{team}"
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-        team_name=data['team_slug']
-        players=data['players']
-        for iplayer in players:
-            player_sc_id=f"{iplayer['slug']}-{iplayer['id']}"
-            player_url=f"https://www.espncricinfo.com/cricketers/{player_sc_id}"
-            driver.get(player_url)
-            time.sleep(30) 
-            try:
-                player_content = driver.find_element(By.ID, "__NEXT_DATA__")
-                player_content = player_content.get_attribute('innerHTML')
-                player_content=json.loads(player_content)
-                player_content=player_content['props']['appPageProps']['data']
-                print('Writing data for '+ team_name)
-                format_and_save_player_data(player_content, team_name)
-            except Exception as e:
-                print(f"Error fetching data for {player_url}: {e}")
-            
+
+     
            
         
 
